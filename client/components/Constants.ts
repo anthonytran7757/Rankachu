91df2d2e86789b4eecf375410bb8fe01091e0cb6
Anthony <anthonytran7757@gmail.com>
2020-09-06 15:27:27 -0700

    constants

---
+++ client/components/Constants.ts
@@ -0,0 +1,6 @@
+export const POKE_URL = "https://pokeapi.co/api/v2/pokemon/"
+export const POKE_SPEC_URL = "https://pokeapi.co/api/v2/pokemon-species/"
+export const GEN_URL = "https://pokeapi.co/api/v2/generation/"
+export const TYPE_URL = "https://pokeapi.co/api/v2/type/"
+export const POKE_SPRITE_URL = "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/versions/generation-viii/icons/"
+export const POKEBALL_SPRITE_URL = "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/items/poke-ball.png"
